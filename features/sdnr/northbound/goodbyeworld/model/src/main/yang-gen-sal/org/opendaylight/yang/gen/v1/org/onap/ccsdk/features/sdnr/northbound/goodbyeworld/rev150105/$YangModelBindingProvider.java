f98418943bafe819467903048f0bb199ba864afd
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2018-11-15 10:12:35 +0100

    Remove distribution target, added .gitignore, build clean

--- features/sdnr/northbound/goodbyeworld/model/src/main/yang-gen-sal/org/opendaylight/yang/gen/v1/org/onap/ccsdk/features/sdnr/northbound/goodbyeworld/rev150105/$YangModelBindingProvider.java
+++
@@ -1,8 +0,0 @@
-package org.opendaylight.yang.gen.v1.org.onap.ccsdk.features.sdnr.northbound.goodbyeworld.rev150105;
-
-public final class $YangModelBindingProvider implements org.opendaylight.yangtools.yang.binding.YangModelBindingProvider {
-    @Override
-    public org.opendaylight.yangtools.yang.binding.YangModuleInfo getModuleInfo() {
-        return $YangModuleInfoImpl.getInstance();
-    }
-}
