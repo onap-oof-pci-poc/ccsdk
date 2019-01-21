fd79fdf0b30562bd7a35014672554aca1a81bde7
highstreetherbert <herbert.eiselt@highstreet-technologies.com>
2019-01-21 09:49:28 +0100

    add first test file for devicemodel

---
+++ features/sdnr/wt/devicemodel/model/src/test/java/org/onap/ccsdk/features/sdnr/wt/devicemodel/test/Test1.java
@@ -0,0 +1,14 @@
+package org.onap.ccsdk.features.sdnr.wt.devicemodel.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
