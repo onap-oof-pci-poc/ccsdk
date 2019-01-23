718ff46f91e59b92b75745bc5a55ffece8f66a71
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-01-23 18:18:55 +0100

    Load test configurations into testcase
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- features/sdnr/wt/websocketmanager2/provider/src/main/java/org/onap/ccsdk/sdnr/wt/websocketmanager2/utils/AkkaConfig.java
+++ features/sdnr/wt/websocketmanager2/provider/src/main/java/org/onap/ccsdk/sdnr/wt/websocketmanager2/utils/AkkaConfig.java
@@ -15,160 +15,173 @@ import com.typesafe.config.ConfigFactory;
 
 public class AkkaConfig{
 
-	private static final Logger LOG = LoggerFactory.getLogger(WebSocketManager.class.getName());
-	
-	public static class ClusterNodeInfo
-	{
-		@Override
-		public String toString() {
-			return "ClusterNodeInfo [protocol=" + protocol + ", clusterName=" + clusterName + ", remoteAdr=" + remoteAdr
-					+ ", port=" + port + "]";
-		}
-		private final String protocol;
-		private final String clusterName;
-		private final String remoteAdr;
-		private final int port;
+    private static final Logger LOG = LoggerFactory.getLogger(WebSocketManager.class.getName());
 
-		public String getRemoteAddress() {return this.remoteAdr;}
-		public ClusterNodeInfo(String s) throws Exception
-		{
-			final String regex ="([a-z.]*):\\/\\/([a-zA-Z0-9-]*)@([a-zA-Z0-9.-]*):([0-9]*)";
-			final Pattern pattern = Pattern.compile(regex);
-			final Matcher matcher = pattern.matcher(s);
-			if(!matcher.find())
-				throw new Exception("invalid seedNode format");
-			this.protocol=matcher.group(1);
-			this.clusterName=matcher.group(2);
-			this.remoteAdr=matcher.group(3);
-			this.port=Integer.parseInt(matcher.group(4));
-		}
-	}
-	public static class ClusterRoleInfo
-	{
-		@Override
-		public String toString() {
-			return "ClusterRoleInfo [Role=" + Role + ", Index=" + Index + "]";
-		}
+    public static class ClusterNodeInfo
+    {
+        @Override
+        public String toString() {
+            return "ClusterNodeInfo [protocol=" + protocol + ", clusterName=" + clusterName + ", remoteAdr=" + remoteAdr
+                    + ", port=" + port + "]";
+        }
+        private final String protocol;
+        private final String clusterName;
+        private final String remoteAdr;
+        private final int port;
 
-		private final String Role;
-		private final int Index;
-		public ClusterRoleInfo(String s) throws Exception {
-			final String regex = "([a-z]*)-([0-9]*)";
-			final Pattern pattern = Pattern.compile(regex);
-			final Matcher matcher = pattern.matcher(s);
-			if(!matcher.find())
-				throw new Exception("invalid role format");
-			this.Role=matcher.group(1);
-			this.Index=Integer.parseInt(matcher.group(2));
-		}
+        public String getRemoteAddress() {return this.remoteAdr;}
+        public ClusterNodeInfo(String s) throws Exception
+        {
+            final String regex ="([a-z.]*):\\/\\/([a-zA-Z0-9-]*)@([a-zA-Z0-9.-]*):([0-9]*)";
+            final Pattern pattern = Pattern.compile(regex);
+            final Matcher matcher = pattern.matcher(s);
+            if(!matcher.find()) {
+                throw new Exception("invalid seedNode format");
+            }
+            this.protocol=matcher.group(1);
+            this.clusterName=matcher.group(2);
+            this.remoteAdr=matcher.group(3);
+            this.port=Integer.parseInt(matcher.group(4));
+        }
+    }
+    public static class ClusterRoleInfo
+    {
+        @Override
+        public String toString() {
+            return "ClusterRoleInfo [Role=" + Role + ", Index=" + Index + "]";
+        }
 
-	}
-	public static class ClusterConfig
-	{
-		@Override
-		public String toString() {
-			return "ClusterConfig [seedNodes=" + seedNodes + ", roles=" + roles + "]";
-		}
-		private final List<ClusterNodeInfo> seedNodes;
-		private final List<ClusterRoleInfo> roles;
-		private final ClusterNodeInfo ismeInfo;
-		public ClusterConfig(Config o) throws Exception {
-			{
-			this.seedNodes = new ArrayList<ClusterNodeInfo>();
-			List<String> a= o.getStringList("seed-nodes");
-			for(int i=0;i<a.size();i++)
-			{
-				ClusterNodeInfo info=new ClusterNodeInfo(a.get(i));
-				this.seedNodes.add(info);
-			}
-			this.roles=new ArrayList<ClusterRoleInfo>();
-			a=o.getStringList("roles");
-			for(int i=0;i<a.size();i++)
-			{
-				ClusterRoleInfo s=new ClusterRoleInfo(a.get(i));
-				this.roles.add(s);
-			}
-			int idx=this.roles.get(0).Index-1;
-			if(idx>=0 && idx<this.seedNodes.size())
-				this.ismeInfo=this.seedNodes.get(idx);
-			else
-				this.ismeInfo=null;
-		}
+        private final String Role;
+        private final int Index;
+        public ClusterRoleInfo(String s) throws Exception {
+            final String regex = "([a-z]*)-([0-9]*)";
+            final Pattern pattern = Pattern.compile(regex);
+            final Matcher matcher = pattern.matcher(s);
+            if(!matcher.find()) {
+                throw new Exception("invalid role format");
+            }
+            this.Role=matcher.group(1);
+            this.Index=Integer.parseInt(matcher.group(2));
+        }
 
-	}
-		public boolean isCluster() {
-			return this.seedNodes!=null?this.seedNodes.size()>1:false;
-		}
-		public boolean isMe(ClusterNodeInfo i) {
-			return this.ismeInfo!=null?this.ismeInfo.equals(i):false;
-		}
-		public List<ClusterNodeInfo> getSeedNodes() {
-			return this.seedNodes;
-		}
-	}
+    }
+    public static class ClusterConfig
+    {
+        @Override
+        public String toString() {
+            return "ClusterConfig [seedNodes=" + seedNodes + ", roles=" + roles + "]";
+        }
+        private final List<ClusterNodeInfo> seedNodes;
+        private final List<ClusterRoleInfo> roles;
+        private final ClusterNodeInfo ismeInfo;
+        public ClusterConfig(Config o) throws Exception {
+            {
+            this.seedNodes = new ArrayList<>();
+            List<String> a= o.getStringList("seed-nodes");
+            for(int i=0;i<a.size();i++)
+            {
+                ClusterNodeInfo info=new ClusterNodeInfo(a.get(i));
+                this.seedNodes.add(info);
+            }
+            this.roles=new ArrayList<>();
+            a=o.getStringList("roles");
+            for(int i=0;i<a.size();i++)
+            {
+                ClusterRoleInfo s=new ClusterRoleInfo(a.get(i));
+                this.roles.add(s);
+            }
+            int idx=this.roles.get(0).Index-1;
+            if(idx>=0 && idx<this.seedNodes.size()) {
+                this.ismeInfo=this.seedNodes.get(idx);
+            } else {
+                this.ismeInfo=null;
+            }
+        }
 
-	private static final String DEFAULT_FILENAME = "configuration/initial/akka.conf";
-	private final File file;
-	private final String resourceFilename;
-	private final String fileContent;
-	private ClusterConfig cluserConfig;
-	public ClusterConfig getClusterConfig() {return this.cluserConfig;}
+    }
+        public boolean isCluster() {
+            return this.seedNodes!=null?this.seedNodes.size()>1:false;
+        }
+        public boolean isMe(ClusterNodeInfo i) {
+            return this.ismeInfo!=null?this.ismeInfo.equals(i):false;
+        }
+        public List<ClusterNodeInfo> getSeedNodes() {
+            return this.seedNodes;
+        }
+    }
 
-	private AkkaConfig(File file,boolean isResource) {
-		this.file=isResource?null:file;
-		this.fileContent=null;
-		this.resourceFilename=isResource?file.getAbsolutePath():null;
-	}
-	private AkkaConfig(String fileContent) {
-		this.file = null;
-		this.fileContent = fileContent;
-		this.resourceFilename = null;
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "AkkaConfig [filename=" + file + ", cluserConfig=" + cluserConfig + "]";
-	}
+    private static final String DEFAULT_FILENAME = "configuration/initial/akka.conf";
+    private final File file;
+    private final String resourceFilename;
+    private final String fileContent;
+    private ClusterConfig cluserConfig;
+    public ClusterConfig getClusterConfig() {return this.cluserConfig;}
 
-	private void loadFromFile() throws Exception {
-		Config cfg=null;
-		if(this.file!=null)
-			cfg=ConfigFactory.parseFile(this.file);
-		else if(this.fileContent!=null)
-			cfg=ConfigFactory.parseString(this.fileContent);
-		else if(this.resourceFilename!=null)
-			cfg=ConfigFactory.parseResources(this.getClass(), this.resourceFilename);
-		
-		if(cfg!=null)
-			this.cluserConfig=new ClusterConfig(cfg.getConfig("odl-cluster-data").getConfig("akka").getConfig("cluster"));
-		else
-		{
-			LOG.warn("unable to parse config file");
-			this.cluserConfig=null;
-		}
-	}
+    private AkkaConfig(File file,boolean isResource) {
+        this.file=isResource?null:file;
+        this.fileContent=null;
+        this.resourceFilename=isResource?file.getName():null;
+    }
+    private AkkaConfig(String fileContent) {
+        this.file = null;
+        this.fileContent = fileContent;
+        this.resourceFilename = null;
+    }
 
-	public boolean isCluster()
-	{
-		return this.cluserConfig!=null?this.cluserConfig.isCluster():false;
-	}
-	public static AkkaConfig load() throws Exception
-	{
-		return load(DEFAULT_FILENAME);
-	}
 
-	public static AkkaConfig load(String filename) throws Exception
-	{
-		return load(filename,false);
-	}
-	public static AkkaConfig load(String filename,boolean isResource) throws Exception
-	{
-		AkkaConfig cfg=new AkkaConfig(new File(filename),isResource);
-		cfg.loadFromFile();
+    @Override
+    public String toString() {
+        return "AkkaConfig [filename=" + file + ", cluserConfig=" + cluserConfig + "]";
+    }
 
-		return cfg;
-	}
+    private void loadFromFile() throws Exception {
+        Config cfg=null;
+        if(this.file!=null) {
+            cfg=ConfigFactory.parseFile(this.file);
+        } else if(this.fileContent!=null) {
+            cfg=ConfigFactory.parseString(this.fileContent);
+        } else if(this.resourceFilename!=null) {
+            cfg=ConfigFactory.parseResources(this.getClass(), this.resourceFilename);
+        }
+
+        if(cfg!=null) {
+            this.cluserConfig=new ClusterConfig(cfg.getConfig("odl-cluster-data").getConfig("akka").getConfig("cluster"));
+        } else
+        {
+            LOG.warn("unable to parse config file");
+            this.cluserConfig=null;
+        }
+    }
+
+    public boolean isCluster()
+    {
+        return this.cluserConfig!=null?this.cluserConfig.isCluster():false;
+    }
+    public static AkkaConfig load() throws Exception
+    {
+        return load(DEFAULT_FILENAME);
+    }
+
+    public static AkkaConfig load(String filename) throws Exception
+    {
+        return load(filename,false);
+    }
+    public static AkkaConfig load(String filename,boolean isResource) throws Exception
+    {
+        AkkaConfig cfg=new AkkaConfig(new File(filename),isResource);
+        cfg.loadFromFile();
+
+        return cfg;
+    }
+    public static AkkaConfig loadContent(String content) throws Exception
+    {
+        AkkaConfig cfg=new AkkaConfig(content);
+        cfg.loadFromFile();
+
+        return cfg;
+    }
+
+
 
 
 
--- features/sdnr/wt/websocketmanager2/provider/src/test/java/org/onap/ccsdk/sdnr/wt/websocketmanager2/test/AkkaConfigTest.java
+++ features/sdnr/wt/websocketmanager2/provider/src/test/java/org/onap/ccsdk/sdnr/wt/websocketmanager2/test/AkkaConfigTest.java
@@ -2,30 +2,56 @@ package org.onap.ccsdk.sdnr.wt.websocketmanager2.test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 import org.junit.Test;
 import org.onap.ccsdk.sdnr.wt.websocketmanager2.utils.AkkaConfig;
 
 public class AkkaConfigTest {
 
-	@Test
-	public void test() {
-		AkkaConfig config=null;
-		try {
-			config = AkkaConfig.load("akka-singlenode.cfg", true);
-		} catch (Exception e) {
-			fail("error loading singlenode config");
-		}
-		assertEquals("no singlenode config detected",false,config.isCluster());
-		assertEquals("more than one node detected",1,config.getClusterConfig().getSeedNodes().size());
-		
-		try {
-			config = AkkaConfig.load("akka-cluster.cfg", true);
-		} catch (Exception e) {
-			fail("error loading cluster config");
-		}
-		assertEquals("no cluster config detected",true,config.isCluster());
-		assertTrue("only one node detected",config.getClusterConfig().getSeedNodes().size()>1);
-	
-	}
+    @Test
+    public void test() throws URISyntaxException, IOException {
 
+        AkkaConfig config=null;
+        try {
+            //config = AkkaConfig.load("akka-singlenode.cfg", true);
+            config = AkkaConfig.loadContent(loadResourceContentAsString("akka-singlenode.cfg"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("error loading singlenode config");
+        }
+        assertEquals("no singlenode config detected",false,config.isCluster());
+        assertEquals("more than one node detected",1,config.getClusterConfig().getSeedNodes().size());
+
+        try {
+            config = AkkaConfig.loadContent(loadResourceContentAsString("akka-cluster.cfg"));
+        } catch (Exception e) {
+            fail("error loading cluster config");
+        }
+        assertEquals("no cluster config detected",true,config.isCluster());
+        assertTrue("only one node detected",config.getClusterConfig().getSeedNodes().size()>1);
+    }
+
+    private String loadResourceContentAsString(String resourceName) throws URISyntaxException, FileNotFoundException, IOException {
+
+        StringBuilder sb = new StringBuilder();
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = Paths.get(classLoader.getResource(resourceName).toURI()).toFile();
+        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = br.readLine();
+            }
+        }
+        return sb.toString();
+    }
 }
