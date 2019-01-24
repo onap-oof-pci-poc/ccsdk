ca918caef13b007a480ea3ed7c37d084c6cffbf5
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-01-24 18:37:36 +0100

    Licenses added to websocketmanager2
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- features/sdnr/wt/websocketmanager2/provider/src/main/java/org/onap/ccsdk/sdnr/wt/websocketmanager2/WebSocketManagerSocket.java
+++ features/sdnr/wt/websocketmanager2/provider/src/main/java/org/onap/ccsdk/sdnr/wt/websocketmanager2/WebSocketManagerSocket.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * ============LICENSE_START=======================================================
+ * ONAP : ccsdk feature sdnr wt websocketmanager2
+ *  ================================================================================
+ * Copyright (C) 2019 highstreet technologies GmbH Intellectual Property.
+ * All rights reserved.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END=========================================================
+ ******************************************************************************/
 package org.onap.ccsdk.sdnr.wt.websocketmanager2;
 
 import java.io.IOException;
@@ -17,13 +37,13 @@ import org.slf4j.LoggerFactory;
 public class WebSocketManagerSocket extends WebSocketAdapter {
 
 	private static final Logger LOG = LoggerFactory.getLogger(WebSocketManagerSocket.class.getName());
-	
+
 	public static final String MSG_KEY_DATA = "data";
 	public static final String MSG_KEY_SCOPES = "scopes";
 	public static final String MSG_KEY_PARAM = "param";
 	public static final String MSG_KEY_VALUE = "value";
 	public static final String MSG_KEY_SCOPE = "scope";
-	
+
 	public static final String KEY_NODENAME = "nodename";
 	public static final String KEY_EVENTTYPE = "eventtype";
 	public static final String KEY_XMLEVENT = "xmlevent";
@@ -31,43 +51,46 @@ public class WebSocketManagerSocket extends WebSocketAdapter {
 	/**
 	 * list of all sessionids
 	 */
-	private static final List<String> sessionIds=new ArrayList<String>();
+	private static final List<String> sessionIds=new ArrayList<>();
 	/**
 	 * map of sessionid <=> UserScopes
 	 */
-	private static final HashMap<String, UserScopes> userScopesList = new HashMap<String, UserScopes>();
+	private static final HashMap<String, UserScopes> userScopesList = new HashMap<>();
 	/**
 	 * map of class.hashCode <=> class
 	 */
-	private static final HashMap<String,WebSocketManagerSocket> clientList=new HashMap<String,WebSocketManagerSocket>();
-	
+	private static final HashMap<String,WebSocketManagerSocket> clientList=new HashMap<>();
+	private final String myUniqueSessionId;
+
+
 	public interface EventInputCallback
 	{
 		void onMessagePushed(final String message) throws Exception;
 	}
-	private final String sessionId;
-	
+
 	public WebSocketManagerSocket() {
-		this.sessionId=_genSessionId();
+		this.myUniqueSessionId=_genSessionId();
 	}
 	@Override
 	protected void finalize() throws Throwable {
-		sessionIds.remove(this.sessionId);
+		sessionIds.remove(this.myUniqueSessionId);
 	}
 	private static String _genSessionId() {
 		Random rnd=new Random();
 		String sid=String.valueOf(rnd.nextLong());
-		while(sessionIds.contains(sid))
+		while(sessionIds.contains(sid)) {
 			sid=String.valueOf(rnd.nextLong());
+		}
 		sessionIds.add(sid);
 		return sid;
 	}
 	@Override
 	public void onWebSocketText(String message) {
 		LOG.info(this.getRemote().getInetSocketAddress().toString()+" has sent "+ message);
-		if(!this.manageClientRequest(message))
+		if(!this.manageClientRequest(message)) {
 			this.manageClientRequest2(message);
-		
+		}
+
 		super.onWebSocketText(message);
 	}
 
@@ -126,8 +149,9 @@ public class WebSocketManagerSocket extends WebSocketAdapter {
 	private void manageClientRequest2(String request) {
 		try {
 			JSONObject o=new JSONObject(request);
-			if(o.has(KEY_NODENAME) && o.has(KEY_EVENTTYPE) && o.has(KEY_EVENTTYPE))
+			if(o.has(KEY_NODENAME) && o.has(KEY_EVENTTYPE) && o.has(KEY_EVENTTYPE)) {
 				broadCast(o.getString(KEY_NODENAME),o.getString(KEY_EVENTTYPE),o.getString(KEY_XMLEVENT));
+			}
 		} catch (Exception e) {
 			LOG.warn("handle ws request failed:"+e.getMessage());
 		}
@@ -141,7 +165,7 @@ public class WebSocketManagerSocket extends WebSocketAdapter {
 	}
 
 	private String getSessionId() {
-		return this.sessionId;
+		return this.myUniqueSessionId;
 	}
 	public static void broadCast(String nodeName, String eventType, String xmlEvent) {
 		if (clientList != null && clientList.size() > 0) {
@@ -160,5 +184,5 @@ public class WebSocketManagerSocket extends WebSocketAdapter {
 		}
 	}
 
-	
+
 }
