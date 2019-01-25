9266196cc66166e9e032ddfdbc329570dd492470
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-01-25 13:27:48 +0100

    Fix comment
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- features/sdnr/wt/odlux/framework/src/services/restService.ts
+++ features/sdnr/wt/odlux/framework/src/services/restService.ts
@@ -10,7 +10,7 @@ export async function requestRest<TData>(path: string = '', init: RequestInit = 
     ...init.headers
   };
   if (authenticate) {
-    #user:admin password:Kp8bJ4SXszM0WXlhak3eHlcse2gAw84vaoGGmJvUy2U
+    /* user:admin password:Kp8bJ4SXszM0WXlhak3eHlcse2gAw84vaoGGmJvUy2U */
     init.headers = {
       ...init.headers,
       'Authorization': 'Basic YWRtaW46S3A4Yko0U1hzek0wV1hsaGFrM2VIbGNzZTJnQXc4NHZhb0dHbUp2VXkyVQ=='
