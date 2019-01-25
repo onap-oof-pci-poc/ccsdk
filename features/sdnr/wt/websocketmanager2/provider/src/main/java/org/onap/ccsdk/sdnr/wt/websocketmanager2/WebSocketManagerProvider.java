605fdf408e66309988ffa60f65f7899ad487388f
highstreetherbert <herbert.eiselt@highstreet-technologies.com>
2019-01-25 10:52:28 +0100

    fixed servlet registration

--- features/sdnr/wt/websocketmanager2/provider/src/main/java/org/onap/ccsdk/sdnr/wt/websocketmanager2/WebSocketManagerProvider.java
+++ features/sdnr/wt/websocketmanager2/provider/src/main/java/org/onap/ccsdk/sdnr/wt/websocketmanager2/WebSocketManagerProvider.java
@@ -20,53 +20,79 @@
  ******************************************************************************/
 package org.onap.ccsdk.sdnr.wt.websocketmanager2;
 
+import javax.servlet.ServletException;
+
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
 import org.opendaylight.controller.sal.binding.api.RpcProviderRegistry;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.websocketmanager.rev150105.WebsocketmanagerService;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WebSocketManagerProvider extends Blueprint {
 
-    private static final Logger LOG = LoggerFactory.getLogger(WebSocketManagerProvider.class);
-    private static final String APPLICATION_NAME = WebSocketManagerProvider.class.getName();
+	private static final Logger LOG = LoggerFactory.getLogger(WebSocketManagerProvider.class);
+	private static final String APPLICATION_NAME = WebSocketManagerProvider.class.getName();
+	private static final String ALIAS = "/websocket";
 
-    private WebSocketManager wsServlet = null;
-    private RpcRegistration<WebsocketmanagerService> websocketService = null;
+	private WebSocketManager wsServlet = null;
+	private RpcRegistration<WebsocketmanagerService> websocketService = null;
+	
+	public WebSocketManagerProvider() {
+		LOG.info("Creating provider for {}", APPLICATION_NAME);
+	}
 
-
-    public WebSocketManagerProvider() {
-        LOG.info( "Creating provider for {}", APPLICATION_NAME);
-    }
-
-    @Override
-    public void init() {
-        LOG.info( "Init provider for {}", APPLICATION_NAME);
-        RpcProviderRegistry rpcProviderRegistry = this.getRpcProviderRegistry();
-        if (rpcProviderRegistry != null) {
-            if (wsServlet != null) {
-				this.websocketService = rpcProviderRegistry.addRpcImplementation(WebsocketmanagerService.class, wsServlet);
+	@Override
+	public void init() {
+		LOG.info("Init provider for {}", APPLICATION_NAME);
+		RpcProviderRegistry rpcProviderRegistry = this.getRpcProviderRegistry();
+		if (rpcProviderRegistry != null) {
+			if (wsServlet != null) {
+				this.websocketService = rpcProviderRegistry.addRpcImplementation(WebsocketmanagerService.class,
+						wsServlet);
 			} else {
-                LOG.error("wsServlet not provided");
-            }
-        } else {
-            LOG.error("rpcProviderRegistry not provided");
-        }
-    }
+				LOG.error("wsServlet not provided");
+			}
+		} else {
+			LOG.error("rpcProviderRegistry not provided");
+		}
+	}
 
-    @Override
-    public void close() throws Exception {
-        LOG.info( "Close provider for {}", APPLICATION_NAME);
-        if (websocketService != null) {
-            websocketService.close();
-        }
-    }
+	@Override
+	public void close() throws Exception {
+		LOG.info("Close provider for {}", APPLICATION_NAME);
+		if (websocketService != null) {
+			websocketService.close();
+		}
+	}
 
-    public WebSocketManager getWsServlet() {
-        return wsServlet;
-    }
+	public void onUnbindService(HttpService httpService) {
+		httpService.unregister(ALIAS);
+		wsServlet = null;
+	}
 
-    public void setWsServlet(WebSocketManager wsServlet) {
-        this.wsServlet = wsServlet;
-    }
+	public void onBindService(HttpService httpService) throws ServletException, NamespaceException {
+		if (httpService == null) {
+			LOG.warn("Unable to inject HttpService into DluxLoader. dlux modules won't work without httpService");
+		} else {
+
+			wsServlet = new WebSocketManager();
+			httpService.registerServlet(ALIAS, wsServlet, null, null);
+			LOG.info("websocket servlet registered.");
+			if(this.websocketService==null)
+				this.init();
+			else
+				LOG.info("websocketservice already initialized");
+		}
+
+	}
+
+	public WebSocketManager getWsServlet() {
+		return wsServlet;
+	}
+
+	public void setWsServlet(WebSocketManager wsServlet) {
+		this.wsServlet = wsServlet;
+	}
 }
