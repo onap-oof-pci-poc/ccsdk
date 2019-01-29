37409ce3d1385004a356e3c1d14466d47c6f9f9d
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-01-29 10:43:36 +0100

    Sonarcube issues fixes
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- features/sdnr/wt/devicemanager/provider/src/main/java/org/onap/ccsdk/features/sdnr/wt/devicemanager/aaiconnector/impl/AaiProviderClient.java
+++ features/sdnr/wt/devicemanager/provider/src/main/java/org/onap/ccsdk/features/sdnr/wt/devicemanager/aaiconnector/impl/AaiProviderClient.java
@@ -32,82 +32,17 @@ public class AaiProviderClient implements AutoCloseable {
 
     private static Logger LOG = LoggerFactory.getLogger(AaiProviderClient.class);
     private static boolean reloadConfigFlag;
+    private static final IConfigChangedListener configChangedListener = () -> reloadConfigFlag = true;
+
     private AaiConfig config;
     private final DeviceManagerImpl deviceManager;
     private final HtDevicemanagerConfiguration htconfig;
-    private final IConfigChangedListener configChangedListener = () -> reloadConfigFlag = true;
 
 
     public AaiConfig getConfig() {
         return this.config;
     }
 
-    private class AaiCreateRequestRunnable implements Runnable {
-
-        private static final int RESPCODE_NOTFOUND = BaseHTTPResponse.CODE404;
-        private static final int RESPCODE_FOUND = BaseHTTPResponse.CODE200;
-        private final AaiWebApiClient mClient;
-        private final String pnfId;
-        private final String type;
-        private final String model;
-        private final String vendor;
-        private final String oamIp;
-        private final List<String> ifaces;
-        private final int timeout;
-
-        public AaiCreateRequestRunnable(String pnfId, String type, String model, String vendor, String oamIp,
-                List<String> ifaces) {
-            this.pnfId = pnfId;
-            this.type = type;
-            this.model = model;
-            this.vendor = vendor;
-            this.oamIp = oamIp;
-            this.ifaces = ifaces;
-            this.timeout = AaiProviderClient.this.config.getConnectionTimeout();
-            this.mClient = new AaiWebApiClient(AaiProviderClient.this.config.getBaseUrl(),
-                    AaiProviderClient.this.config.getHeaders(), AaiProviderClient.this.config.getTrustAll(),
-                    AaiProviderClient.this.config.getPcks12CertificateFilename(),
-                    AaiProviderClient.this.config.getPcks12CertificatePassphrase());
-        }
-
-        @Override
-        public void run() {
-            LOG.debug("check if pnfid " + pnfId + " exists");
-            this.mClient.setTimeout(timeout);
-            BaseHTTPResponse response = this.mClient.pnfCheckIfExists(pnfId);
-            if (response.code == RESPCODE_NOTFOUND) {
-                LOG.debug("do pnfCreate for " + pnfId);
-                this.mClient.pnfCreate(pnfId, type, model, vendor, oamIp, ifaces);
-            } else if (response.code == RESPCODE_FOUND) {
-                LOG.debug("pnfid " + pnfId + " found, nothing to do");
-            } else {
-                LOG.warn("unhandled response code: " + response.toString());
-            }
-        }
-    };
-
-    private class AaiDeleteRequestRunnable implements Runnable {
-
-        private final AaiWebApiClient mClient;
-        private final String pnfId;
-        private final int timeout;
-
-
-        public AaiDeleteRequestRunnable(String pnfId) {
-            this.pnfId = pnfId;
-            this.timeout = AaiProviderClient.this.config.getConnectionTimeout();
-            this.mClient = new AaiWebApiClient(AaiProviderClient.this.config.getBaseUrl(),
-                    AaiProviderClient.this.config.getHeaders(), AaiProviderClient.this.config.getTrustAll(),
-                    AaiProviderClient.this.config.getPcks12CertificateFilename(),
-                    AaiProviderClient.this.config.getPcks12CertificatePassphrase());
-        }
-
-        @Override
-        public void run() {
-            this.mClient.setTimeout(this.timeout);
-            this.mClient.pnfDelete(pnfId);
-        }
-    };
 
     public AaiProviderClient(HtDevicemanagerConfiguration cfg, DeviceManagerImpl devMgr) {
         this.config = cfg.getAai();
@@ -154,7 +89,7 @@ public class AaiProviderClient implements AutoCloseable {
         if (this.config.doDeleteOnMountPointRemoved()) {
             new Thread(new AaiDeleteRequestRunnable(mountPointName)).start();
         } else {
-            LOG.debug("prevent deleting device " + mountPointName + " by config");
+            LOG.debug("prevent deleting device {} by config", mountPointName);
         }
     }
 
@@ -162,4 +97,72 @@ public class AaiProviderClient implements AutoCloseable {
     public void close() throws Exception {
         this.htconfig.unregisterConfigChangedListener(configChangedListener);
     }
+
+    private class AaiCreateRequestRunnable implements Runnable {
+
+        private static final int RESPCODE_NOTFOUND = BaseHTTPResponse.CODE404;
+        private static final int RESPCODE_FOUND = BaseHTTPResponse.CODE200;
+        private final AaiWebApiClient mClient;
+        private final String pnfId;
+        private final String type;
+        private final String model;
+        private final String vendor;
+        private final String oamIp;
+        private final List<String> ifaces;
+        private final int timeout;
+
+        public AaiCreateRequestRunnable(String pnfId, String type, String model, String vendor, String oamIp,
+                List<String> ifaces) {
+            this.pnfId = pnfId;
+            this.type = type;
+            this.model = model;
+            this.vendor = vendor;
+            this.oamIp = oamIp;
+            this.ifaces = ifaces;
+            this.timeout = AaiProviderClient.this.config.getConnectionTimeout();
+            this.mClient = new AaiWebApiClient(AaiProviderClient.this.config.getBaseUrl(),
+                    AaiProviderClient.this.config.getHeaders(), AaiProviderClient.this.config.getTrustAll(),
+                    AaiProviderClient.this.config.getPcks12CertificateFilename(),
+                    AaiProviderClient.this.config.getPcks12CertificatePassphrase());
+        }
+
+        @Override
+        public void run() {
+            LOG.debug("check if pnfid {} exists", pnfId);
+            this.mClient.setTimeout(timeout);
+            BaseHTTPResponse response = this.mClient.pnfCheckIfExists(pnfId);
+            if (response.code == RESPCODE_NOTFOUND) {
+                LOG.debug("do pnfCreate for {}", pnfId);
+                this.mClient.pnfCreate(pnfId, type, model, vendor, oamIp, ifaces);
+            } else if (response.code == RESPCODE_FOUND) {
+                LOG.debug("pnfid {} found, nothing to do", pnfId);
+            } else {
+                LOG.warn("unhandled response code: {}", response.toString());
+            }
+        }
+    };
+
+    private class AaiDeleteRequestRunnable implements Runnable {
+
+        private final AaiWebApiClient mClient;
+        private final String pnfId;
+        private final int timeout;
+
+
+        public AaiDeleteRequestRunnable(String pnfId) {
+            this.pnfId = pnfId;
+            this.timeout = AaiProviderClient.this.config.getConnectionTimeout();
+            this.mClient = new AaiWebApiClient(AaiProviderClient.this.config.getBaseUrl(),
+                    AaiProviderClient.this.config.getHeaders(), AaiProviderClient.this.config.getTrustAll(),
+                    AaiProviderClient.this.config.getPcks12CertificateFilename(),
+                    AaiProviderClient.this.config.getPcks12CertificatePassphrase());
+        }
+
+        @Override
+        public void run() {
+            this.mClient.setTimeout(this.timeout);
+            this.mClient.pnfDelete(pnfId);
+        }
+    };
+
 }
