1732ce96f6a6e55c9bb13e87d18b9c3c68bf6af4
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-01-29 14:34:53 +0100

    Testcase added.
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- features/sdnr/wt/helpserver/provider/src/test/java/TestHelpServer.java
+++ features/sdnr/wt/helpserver/provider/src/test/java/TestHelpServer.java
@@ -6,31 +6,29 @@
  * =================================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  * ============LICENSE_END==========================================================================
  ******************************************************************************/
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
-
-/*
- * Copyright (c) 2019 Red Hat, Inc. and others. All rights reserved.
- *
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution,
- * and is available at http://www.eclipse.org/legal/epl-v10.html
- */
+import org.onap.ccsdk.features.sdnr.wt.helpserver.HelpServlet;
 
 public class TestHelpServer {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void test() throws Exception {
+
+        HelpServlet helpServelet = new HelpServlet();
+
+        assertNotNull("Helpservelet created", helpServelet);
+
+        helpServelet.close();
     }
 
 }
