ee580533a8975d5ac0d079f07b216d0d37a08f0e
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-02-19 19:07:35 +0100

    License fix
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- features/sdnr/wt/websocketmanager2/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/websocketmanager2/test/TestWebsocketClient.java
+++ features/sdnr/wt/websocketmanager2/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/websocketmanager2/test/TestWebsocketClient.java
@@ -1,3 +1,20 @@
+/*******************************************************************************
+ * ============LICENSE_START========================================================================
+ * ONAP : ccsdk feature sdnr wt
+ * =================================================================================================
+ * Copyright (C) 2019 highstreet technologies GmbH Intellectual Property. All rights reserved.
+ * =================================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ * ============LICENSE_END==========================================================================
+ ******************************************************************************/
 package org.onap.ccsdk.features.sdnr.wt.websocketmanager2.test;
 
 import static org.junit.Assert.*;
@@ -9,9 +26,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
-import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
-import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
-import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 import org.java_websocket.handshake.ServerHandshake;
 import org.junit.After;
@@ -24,93 +38,90 @@ import org.onap.ccsdk.features.sdnr.wt.websocketmanager2.websocket.SyncWebSocket
 
 public class TestWebsocketClient {
 
-	private static final int PORT = 44242;
-	Server server;
-	SyncWebSocketClient clientToTest = null;
-	
-	@Test
-	public void test() {
-		try {
-			clientToTest = new SyncWebSocketClient(String.format("ws://127.0.0.1:%d/websocket", PORT));
+    private static final int PORT = 44242;
+    Server server;
+    SyncWebSocketClient clientToTest = null;
 
-		} catch (URISyntaxException e) {
-			fail(e.getMessage());
-		}
-		if (clientToTest != null) {
-			clientToTest.addEventHandler(new WebsocketEventHandler() {
-				
-				@Override
-				public void onOpen(ServerHandshake arg0) {
-					System.out.println("channel opened");
-					clientToTest.send("abc");
-				}
-				
-				@Override
-				public void onMessageReceived(String message) {
-					System.out.println(String.format("received message: %s",message));
-					
-				}
-				
-				@Override
-				public void onError(Exception e) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void onClose(int arg0, String arg1, boolean arg2) {
-					System.out.println("channel closed");
-					
-				}
-			});
-			
+    @Test
+    public void test() {
+        try {
+            clientToTest = new SyncWebSocketClient(String.format("ws://127.0.0.1:%d/websocket", PORT));
 
-			clientToTest.connect();
-			
-		}
+        } catch (URISyntaxException e) {
+            fail(e.getMessage());
+        }
+        if (clientToTest != null) {
+            clientToTest.addEventHandler(new WebsocketEventHandler() {
 
-	}
+                @Override
+                public void onOpen(ServerHandshake arg0) {
+                    System.out.println("channel opened");
+                    clientToTest.send("abc");
+                }
 
-	@Before
-	public void init() {
-		this.server = new Server(new InetSocketAddress("127.0.0.1",PORT));
-		WebSocketHandler wshandler1 = new org.eclipse.jetty.websocket.server.WebSocketHandler() {
-			@Override
-			public void configure(WebSocketServletFactory factory) {
-				factory.setCreator(new WebSocketCreator() {
-					@Override
-					public Object createWebSocket(ServletUpgradeRequest req, ServletUpgradeResponse resp) {
-						if (req.getSubProtocols().size() > 0) {
-							resp.setAcceptedSubProtocol(req.getSubProtocols().get(0));
-						}
-						return new WebSocketManagerSocket();
-					}
-				});
-			}
-		};
-		HandlerList handlers = new HandlerList();
-		ServletContextHandler wshandler2 = new ServletContextHandler(server, "/");
-		wshandler2.addServlet(WebSocketManager.class, "/websocket");
-		handlers.setHandlers(new Handler[] { wshandler1, wshandler2 });
-		server.setHandler(handlers);
-		try {
-			server.start();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+                @Override
+                public void onMessageReceived(String message) {
+                    System.out.println(String.format("received message: %s",message));
 
-	@After
-	public void deinit() {
-		if (server != null) {
-			try {
-				server.stop();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	}
+                }
+
+                @Override
+                public void onError(Exception e) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void onClose(int arg0, String arg1, boolean arg2) {
+                    System.out.println("channel closed");
+
+                }
+            });
+
+
+            clientToTest.connect();
+
+        }
+
+    }
+
+    @Before
+    public void init() {
+        this.server = new Server(new InetSocketAddress("127.0.0.1",PORT));
+        WebSocketHandler wshandler1 = new org.eclipse.jetty.websocket.server.WebSocketHandler() {
+            @Override
+            public void configure(WebSocketServletFactory factory) {
+                factory.setCreator((req, resp) -> {
+                    if (req.getSubProtocols().size() > 0) {
+                        resp.setAcceptedSubProtocol(req.getSubProtocols().get(0));
+                    }
+                    return new WebSocketManagerSocket();
+                });
+            }
+        };
+        HandlerList handlers = new HandlerList();
+        ServletContextHandler wshandler2 = new ServletContextHandler(server, "/");
+        wshandler2.addServlet(WebSocketManager.class, "/websocket");
+        handlers.setHandlers(new Handler[] { wshandler1, wshandler2 });
+        server.setHandler(handlers);
+        try {
+            server.start();
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void deinit() {
+        if (server != null) {
+            try {
+                server.stop();
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
 
 }
--- features/sdnr/wt/websocketmanager2/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/websocketmanager2/test/WebsocketServerConnectTest.java
+++ features/sdnr/wt/websocketmanager2/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/websocketmanager2/test/WebsocketServerConnectTest.java
@@ -6,9 +6,9 @@
  * =================================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -17,10 +17,14 @@
  ******************************************************************************/
 package org.onap.ccsdk.features.sdnr.wt.websocketmanager2.test;
 
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.servlet.ServletContextHandler;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.util.concurrent.ExecutionException;
 import org.eclipse.jetty.websocket.api.WebSocketPolicy;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 import org.json.JSONObject;
@@ -33,75 +37,64 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.websocke
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.websocketmanager.rev150105.WebsocketEventOutput;
 import org.opendaylight.yangtools.yang.common.RpcResult;
 
-import com.google.common.util.concurrent.ListenableFuture;
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-import java.util.concurrent.ExecutionException;
-
 public class WebsocketServerConnectTest {
 
-	private static final String XML1 = "<notification></notification>";
-	private static final String NODENAME = "abc";
-	private static final String EVENTTYPE = "test";
-	protected boolean responseReceived;
+    private static final String XML1 = "<notification></notification>";
+    private static final String NODENAME = "abc";
+    private static final String EVENTTYPE = "test";
+    protected boolean responseReceived;
 
-	@Test
-	public void test() {
-		responseReceived = false;
-		AkkaConfig config = null;
-		try {
-			// config = AkkaConfig.load("akka-singlenode.cfg", true);
-			config = AkkaConfig.loadContent(AkkaConfigTest.loadResourceContentAsString("akka-cluster-local.cfg"));
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail("error loading singlenode config");
-		}
-		EventInputCallback callback = new EventInputCallback() {
+    @Test
+    public void test() {
+        responseReceived = false;
+        AkkaConfig config = null;
+        try {
+            // config = AkkaConfig.load("akka-singlenode.cfg", true);
+            config = AkkaConfig.loadContent(AkkaConfigTest.loadResourceContentAsString("akka-cluster-local.cfg"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("error loading singlenode config");
+        }
+        EventInputCallback callback = message -> {
+            JSONObject o = new JSONObject(message);
+            assertEquals("message which was pushed is not as expected", XML1,
+                    o.get(WebSocketManagerSocket.KEY_XMLEVENT));
+            assertEquals("nodename which was pushed is not as expected", NODENAME,
+                    o.get(WebSocketManagerSocket.KEY_NODENAME));
+            assertEquals("eventtype which was pushed is not as expected", EVENTTYPE,
+                    o.get(WebSocketManagerSocket.KEY_EVENTTYPE));
+            responseReceived = true;
+        };
+        WebSocketManager servlet = new WebSocketManager(config, callback);
+        WebsocketEventInput input = mock(WebsocketEventInput.class);
+        when(input.getXmlEvent()).thenReturn(XML1);
+        when(input.getNodeName()).thenReturn(NODENAME);
+        when(input.getEventType()).thenReturn(EVENTTYPE);
+        ListenableFuture<RpcResult<WebsocketEventOutput>> result = servlet.websocketEvent(input);
+        assertNotNull(result);
+        RpcResult<WebsocketEventOutput> rpc = null;
+        try {
+            rpc = result.get();
+        } catch (InterruptedException | ExecutionException e) {
+            fail(e.getMessage());
+        }
+        assertNotNull(rpc);
+        assertTrue("rpc result was not successful", rpc.isSuccessful());
+        assertTrue(rpc.getResult().getResponse().equals("OK"));
+        while (!responseReceived) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
 
-			@Override
-			public void onMessagePushed(String message) throws Exception {
-				JSONObject o = new JSONObject(message);
-				assertEquals("message which was pushed is not as expected", XML1,
-						o.get(WebSocketManagerSocket.KEY_XMLEVENT));
-				assertEquals("nodename which was pushed is not as expected", NODENAME,
-						o.get(WebSocketManagerSocket.KEY_NODENAME));
-				assertEquals("eventtype which was pushed is not as expected", EVENTTYPE,
-						o.get(WebSocketManagerSocket.KEY_EVENTTYPE));
-				responseReceived = true;
-			}
-		};
-		WebSocketManager servlet = new WebSocketManager(config, callback);
-		WebsocketEventInput input = mock(WebsocketEventInput.class);
-		when(input.getXmlEvent()).thenReturn(XML1);
-		when(input.getNodeName()).thenReturn(NODENAME);
-		when(input.getEventType()).thenReturn(EVENTTYPE);
-		ListenableFuture<RpcResult<WebsocketEventOutput>> result = servlet.websocketEvent(input);
-		assertNotNull(result);
-		RpcResult<WebsocketEventOutput> rpc = null;
-		try {
-			rpc = result.get();
-		} catch (InterruptedException | ExecutionException e) {
-			fail(e.getMessage());
-		}
-		assertNotNull(rpc);
-		assertTrue("rpc result was not successful", rpc.isSuccessful());
-		assertTrue(rpc.getResult().getResponse().equals("OK"));
-		while (!responseReceived) {
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-				e.printStackTrace();
-			}
-		}
+        WebSocketServletFactory factory = mock(WebSocketServletFactory.class);
+        WebSocketPolicy wspolicy = mock(WebSocketPolicy.class);
+        when(factory.getPolicy()).thenReturn(wspolicy);
+        servlet.configure(factory);
 
-		WebSocketServletFactory factory = mock(WebSocketServletFactory.class);
-		WebSocketPolicy wspolicy = mock(WebSocketPolicy.class);
-		when(factory.getPolicy()).thenReturn(wspolicy);
-		servlet.configure(factory);
-
-	}
+    }
 
 }
