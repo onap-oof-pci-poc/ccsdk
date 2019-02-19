3215fcffb6c7ac6856f2253db698d6b05dabe67d
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-02-19 18:18:32 +0100

    Fix for testcase
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- features/sdnr/wt/odlux/core/provider/src/test/java/org/onap/ccsdk/features/sdnr/odlux/test/TestLoadResources.java
+++ features/sdnr/wt/odlux/core/provider/src/test/java/org/onap/ccsdk/features/sdnr/odlux/test/TestLoadResources.java
@@ -6,9 +6,9 @@
  * =================================================================================================
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -26,21 +26,21 @@ import org.onap.ccsdk.features.sdnr.wt.odlux.ResFilesServlet;
 
 public class TestLoadResources {
 
-	@Test
-	public void test() {
-		ResFilesServlet servlet = new ResFilesServlet();
-		String indexhtml=null;
-		indexhtml=servlet.loadFileContent("odlux/index.html");
-		assertNotNull(indexhtml);
-		final String regex = "require\\(\\[\"run\".*\\]";
-		final Pattern pattern = Pattern.compile(regex,Pattern.MULTILINE);
-		final Matcher matcher = pattern.matcher(indexhtml);
-		System.out.println(indexhtml);
-		assertTrue(matcher.find());
-	}
-	@Test
-	public void test2() {
-		
-	}
+    @Test
+    public void test() {
+        ResFilesServlet servlet = new ResFilesServlet();
+        String indexhtml=null;
+        indexhtml=servlet.loadFileContent("odlux/index.html");
+        assertNotNull(indexhtml);
+        final String regex = "require\\(\\[.*\"run\".*\\]";
+        final Pattern pattern = Pattern.compile(regex,Pattern.MULTILINE);
+        System.out.println(indexhtml);
+        final Matcher matcher = pattern.matcher(indexhtml);
+        assertTrue("Can not find patter '"+regex+"'",matcher.find());
+    }
+    @Test
+    public void test2() {
+
+    }
 
 }
