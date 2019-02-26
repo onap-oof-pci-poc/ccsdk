bc2ef3c13bfda8b5b516e771475fa43a0bedd6ec
Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>
2019-02-26 07:14:09 +0100

    Write msg for test fail into jenkins log.
    Signed-off-by: Herbert Eiselt <herbert.eiselt@highstreet-technologies.com>

--- features/sdnr/wt/devicemanager/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/devicemanager/test/TestDeviceManagerWithDatabase.java
+++ features/sdnr/wt/devicemanager/provider/src/test/java/org/onap/ccsdk/features/sdnr/wt/devicemanager/test/TestDeviceManagerWithDatabase.java
@@ -25,6 +25,8 @@ import static org.junit.Assert.fail;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -89,22 +91,26 @@ public class TestDeviceManagerWithDatabase {
         RpcProviderRegistry rpcProviderRegistry = new RpcProviderRegistryMock();
 
         //start using blueprint interface
-        deviceManager = new DeviceManagerImpl();
-
-        deviceManager.setDataBroker(dataBrokerNetconf);
-        deviceManager.setMountPointService(mountPointService);
-        deviceManager.setNotificationPublishService(notificationPublishService);
-        deviceManager.setRpcProviderRegistry(rpcProviderRegistry);
-
+        String msg = "";
         try {
+            deviceManager = new DeviceManagerImpl();
+
+            deviceManager.setDataBroker(dataBrokerNetconf);
+            deviceManager.setMountPointService(mountPointService);
+            deviceManager.setNotificationPublishService(notificationPublishService);
+            deviceManager.setRpcProviderRegistry(rpcProviderRegistry);
+
             deviceManager.init();
         } catch (Exception e) {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            e.printStackTrace(pw);
+            msg = sw.toString(); // stack trace as a string
             e.printStackTrace();
         }
-
         readOnlyTransaction.close();
         System.out.println("Initialization status: "+deviceManager.isDevicemanagerInitializationOk());
-        assertTrue("Devicemanager not initialized", deviceManager.isDevicemanagerInitializationOk());
+        assertTrue("Devicemanager not initialized: "+msg, deviceManager.isDevicemanagerInitializationOk());
         System.out.println("Initialization done");
 
     }
